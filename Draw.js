590a94c8cb9e470dbb8261c1c1dc93422980acf4
g-strzelecki <grzegorz.strzelecki84@gmail.com>
2020-04-03 21:57:22 +0200

    initial commit

---
+++ Draw.js
@@ -0,0 +1,17 @@
+class Draw {
+  constructor() {
+    this.options = ['red', 'green', 'blue'];
+    let _result = this.DrawResult();
+    this.getDrawResult = () => _result;
+  }
+
+  DrawResult() {
+    let colors = [];
+    for (let i = 0; i < this.options.length; i++) {
+      const index = Math.floor(Math.random() * this.options.length);
+      const color = this.options[index];
+      colors.push(color);
+    }
+    return colors;
+  }
+}
